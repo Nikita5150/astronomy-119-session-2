66b9d5242e2d42c6674537239f43d5fb39d8b86d
Nikita5150 <43654655+Nikita5150@users.noreply.github.com>
2018-10-02 13:08:39 -0700

    Added scripts to ASTR119 session 2
    A bunch of simple Python

---
+++ operators.py
@@ -0,0 +1,37 @@
+x = 9
+y = 3	#integers
+
+#arithmetic operators
+print(x+y)	#addition
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)	#modulus {gives remainder /}
+print(x**y) #exponentiation {takes x to power of y}
+x = 9.1918123
+print(x//y) #floor division {how many y in x rounded to closest to number}
+
+#assignment operators
+x = 9 		#sets x equal to 9 {changes the value of x}
+#x == 9		#checks the value of x {no change}
+x += 3		#x = x+3
+print(x)
+x = 9
+x -= 3		#x= x-3
+print(x)
+x *= 3		#x = x * 3
+print(x)
+x /= 3		#x =x /3
+print(x)
+x **= 3		#x to power of 3
+print(x)
+
+#Comparison operators
+x = 9
+y = 3
+print(x==y)		#True if x = y, False sinon
+print(x!=y)		#true if x not equal y
+print(x>y)
+print(x<y)
+print(x>=y)		#True if x greater than or equal to y
+print(x<=y)		#True if x smaller or equal
